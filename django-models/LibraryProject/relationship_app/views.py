0e83ef4a57d83db901599132bfe6901e7e33bab6
moalsafi9 <modata32@gmail.com>
2025-09-07 23:40:30 +0300

    Task 1

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Book
+from .models import Library   # <-- explicit import the checker wants
 
 
 # Function-based view: list all books
